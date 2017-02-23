b4a3d8384dd9103dd3d4e94bd53ae6d38ebd7aa4
Kim Nobay <kim@nobay.co.uk>
2017-02-23 13:49:40 +0000

    Kims original tourtuss demo for seed camp bid

---
+++ experience-detail.php
@@ -0,0 +1,253 @@
+<?php include ('template-top.php'); ?>
+
+
+<header>
+
+      <div class="row">
+            <div class="col-12 col-d-12">
+                </span><span class="destination-name">Colonial history walking tour</span>
+                <br>
+                <span class="destination-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>         
+            </div>
+
+    </div>
+
+</header>
+    <main>
+       
+    </div>
+    
+        <div class="row divider">
+            <div class="col-12 col-t-12 col-d-12">&nbsp;</div></a>
+        </div>
+
+
+
+    <div class="row">
+  <div class="col-1 col-d-1">&nbsp;</div>
+
+            <div class="col-10 col-d-10 experience-description">
+Lunchtime walking tour of hidden colonial buildings near the park with friendly local archaeologist Lisa. Best for photographers and historians.
+  </div>
+  <div class="col-1 col-d-1">&nbsp;</div>
+    </div>
+    
+
+
+    
+        <div class="row divider">
+            <div class="col-12 col-t-12 col-d-12">&nbsp;</div></a>
+        </div>
+
+
+    <div class="row">
+  <div class="col-1 col-d-1">&nbsp;</div>
+
+            <div class="col-10 col-d-10">
+
+
+<style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;border:none;}
+.tg td{font-family:Arial, sans-serif;font-size:14px;padding:2px 2px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;}
+.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:2px 2px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;}
+.tg .tg-dx8v{font-size:12px;vertical-align:top;text-align:left}
+@media screen and (max-width: 767px) {.tg {width: auto !important;}.tg col {width: auto !important;}.tg-wrap {overflow-x: auto;-webkit-overflow-scrolling: touch;}}</style>
+<div class="tg-wrap"><table class="tg" style="undefined;table-layout: fixed; width: 1260px">
+<colgroup>
+<col style="width: 153px; ">
+<col style="width: 1107px;">
+</colgroup>
+
+
+  <tr>
+    <td class="tg-dx8v">Language</td>
+    <td class="tg-dx8v">English</td>
+  </tr>
+  <tr>
+    <td class="tg-dx8v">Operator</td>
+    <td class="tg-dx8v">Maria Sanchez Tours, Pallermo</td>
+  </tr>
+  <tr>
+    <td class="tg-dx8v">When</td>
+    <td class="tg-dx8v">13:00, 17 February 2017</td>
+  </tr>
+  <tr>
+    <td class="tg-dx8v">Meet</td>
+    <td class="tg-dx8v">San Cristóbal 460, Recoleta, Región Metropolitana, Chile</td>
+  </tr>
+  <tr>
+    <td class="tg-dx8v">Duration</td>
+    <td class="tg-dx8v">1 hour</td>
+  </tr>
+  <tr>
+    <td class="tg-dx8v">Price</td>
+    <td class="tg-dx8v">$8.00 per person</td>
+  </tr>
+  <tr>
+    <td class="tg-dx8v">Places</td>
+    <td class="tg-dx8v">3 of 8 available</td>
+  </tr>
+ <!-- <tr>
+    <td class="tg-dx8v">Special</td>
+    <td class="tg-dx8v">There is a large hill with 146 steps that we walk up in the park to finish at the cafe for lunch. The cafe serve meals and drinks including alcohol. People joining must be physically fit enough to walk up the steps or make their own way to the cafe. There are no vegetarian options at the cafe.</td>
+  </tr>
+
+  -->
+</table></div>
+
+
+
+</div>
+  <div class="col-1 col-d-1">&nbsp;</div>
+</div>
+    
+
+ <div class="row">
+            <div class="col-6 col-d-6"><button class="button join"><a href="experience-booking.php"><i class="fa fa-shopping-basket fa-2x"></i>+</a></button> 
+             </div>
+            <div class="col-6 col-d-6"><button class="button maybe"><a href="bookings.php"><i class="fa fa-lightbulb-o fa-2x"></i></a>+</button> 
+             </div>
+</div>
+
+
+
+        <div class="row divider">
+            <div class="col-12 col-t-12 col-d-12">&nbsp;</div></a>
+        </div>
+
+  <div class="row">
+            <div class="col-12 col-d-12">
+                </span><span class="destination-name">Guide</span>
+             </div>
+    </div>      
+
+
+ <div class="row">
+   <div class="col-1 col-d-1">&nbsp;</div>
+            <div class="col-3 col-d-3 left listing-profile">
+                <a href="profile.php"><img src="images/lisa.png"></a>
+                <br>
+                 Lisa Garcia Sanchez
+            </div>
+
+
+            <div class="col-7 col-d-7 left">
+
+                <small>Speaks Spanish, English & Italian</small>
+                <br><br>
+                <i class="fa fa-smile-o fa-2x"></i><span class="smile-count">3452</span><i class="fa fa-meh-o fa-2x"></i><span class="smile-count">131</span><i class="fa fa-frown-o fa-2x"></i><span class="smile-count">131</span>
+                <i class="fa fa-camera fa-2x"></i><span class="smile-count">110</span>&nbsp;<i class="fa fa-university fa-2x"></i><span class="smile-count">50</span>
+
+            </div>
+      <div class="col-1 col-d-1">&nbsp;</div>
+ </div>
+
+ 
+
+
+        <div class="row divider">
+            <div class="col-12 col-t-12 col-d-12">&nbsp;</div></a>
+        </div>
+
+  <div class="row">
+            <div class="col-12 col-d-12">
+                </span><span class="destination-name">Going!</span>
+             </div>
+    </div>      
+
+
+ <div class="row">
+   <div class="col-1 col-d-1">&nbsp;</div>
+            <div class="col-3 col-d-3 left listing-profile">
+                <a href="profile.php"><img src="images/victor.png"></a>
+                <br>
+                SammyT + 1
+            </div>
+
+
+            <div class="col-7 col-d-7 left">
+
+                <small>Speaks French, English & Russian</small>
+                <br><br>
+                <i class="fa fa-smile-o fa-1x"></i><span class="smile-count">52</span>&nbsp;<i class="fa fa-meh-o fa-1x"></i><span class="smile-count">11</span></i>&nbsp;<i class="fa fa-beer fa-1x"></i><span class="smile-count">6</span>&nbsp;&nbsp;<i class="fa fa-university fa-1x"></i><span class="smile-count">12</span>&nbsp;<span class="flag-icon flag-icon-gb"></span>&nbsp;<span class="flag-icon flag-icon-cl"></span>&nbsp;<span class="flag-icon flag-icon-gr"></span>&nbsp;<span class="flag-icon flag-icon-es"></span>&nbsp;<span class="flag-icon flag-icon-br"></span>&nbsp;&nbsp;<span class="flag-icon flag-icon-au"></span>&nbsp;&nbsp;<span class="flag-icon flag-icon-no"></span>&nbsp;&nbsp;<span class="flag-icon flag-icon-de"></span>&nbsp;
+
+            </div>
+      <div class="col-1 col-d-1">&nbsp;</div>
+ </div>
+
+  <div class="row">
+   <div class="col-1 col-d-1">&nbsp;</div>
+            <div class="col-3 col-d-3 left listing-profile">
+                   <a href="profile.php"><img src="images/maria.png"></a>
+                <br>
+                Maria97
+            </div>
+
+
+            <div class="col-7 col-d-7 left">
+
+                <small>Speaks Spanish & English</small>
+                <br><br>
+                <i class="fa fa-meh-o fa-1x"></i><span class="smile-count">50</span>&nbsp;<i class="fa fa-frown-o fa-1x"></i><span class="smile-count">21</span></i>&nbsp;<i class="fa fa-university fa-1x"></i><span class="smile-count">6</span>&nbsp;&nbsp;<span class="flag-icon flag-icon-es"></span>&nbsp;<span class="flag-icon flag-icon-br"></span>&nbsp;&nbsp;<span class="flag-icon flag-icon-au"></span>&nbsp;&nbsp;<span class="flag-icon flag-icon-no"></span>&nbsp;&nbsp;<span class="flag-icon flag-icon-de"></span>&nbsp;
+
+            </div>
+      <div class="col-1 col-d-1">&nbsp;</div>
+ </div>
+
+
+
+        <div class="row divider">
+            <div class="col-12 col-t-12 col-d-12">&nbsp;</div></a>
+        </div>
+
+  <div class="row">
+            <div class="col-12 col-d-12">
+                </span><span class="destination-name">Maybe...</span>
+             </div>
+    </div>      
+
+
+ <div class="row">
+   <div class="col-1 col-d-1">&nbsp;</div>
+            <div class="col-3 col-d-3 left listing-profile">
+                           <a href="profile.php"><img src="images/rudi.png"></a>
+                <br>
+                Ruinuvo
+            </div>
+
+
+            <div class="col-7 col-d-7 left">
+
+                <small>Speaks English & Swahili</small>
+                <br><br>
+                <i class="fa fa-smile-o fa-1x"></i><span class="smile-count">52</span>&nbsp;<i class="fa fa-meh-o fa-1x"></i><span class="smile-count">11</span></i>&nbsp;<i class="fa fa-beer fa-1x"></i><span class="smile-count">6</span>&nbsp;&nbsp;<i class="fa fa-university fa-1x"></i><span class="smile-count">12</span>&nbsp;<span class="flag-icon flag-icon-gb"></span>&nbsp;<span class="flag-icon flag-icon-cl"></span>&nbsp;<span class="flag-icon flag-icon-gr"></span>&nbsp;<span class="flag-icon flag-icon-es"></span>&nbsp;<span class="flag-icon flag-icon-br"></span>&nbsp;&nbsp;<span class="flag-icon flag-icon-au"></span>&nbsp;&nbsp;<span class="flag-icon flag-icon-no"></span>&nbsp;&nbsp;<span class="flag-icon flag-icon-de"></span>&nbsp;
+
+            </div>
+      <div class="col-1 col-d-1">&nbsp;</div>
+ </div>
+
+  <div class="row">
+   <div class="col-1 col-d-1">&nbsp;</div>
+            <div class="col-3 col-d-3 left listing-profile">
+                               <a href="profile.php"><img src="images/valentina.png"></a>
+                <br>
+                Valeski
+            </div>
+
+
+            <div class="col-7 col-d-7 left">
+
+                <small>Speaks Italian, German, Spanish French & English</small>
+                <br><br>
+                <i class="fa fa-meh-o fa-1x"></i><span class="smile-count">50</span>&nbsp;<i class="fa fa-frown-o fa-1x"></i><span class="smile-count">21</span></i>&nbsp;<i class="fa fa-university fa-1x"></i><span class="smile-count">6</span>&nbsp;&nbsp;<span class="flag-icon flag-icon-es"></span>&nbsp;<span class="flag-icon flag-icon-br"></span>&nbsp;&nbsp;<span class="flag-icon flag-icon-au"></span>&nbsp;&nbsp;<span class="flag-icon flag-icon-no"></span>&nbsp;&nbsp;<span class="flag-icon flag-icon-de"></span>&nbsp;
+
+            </div>
+      <div class="col-1 col-d-1">&nbsp;</div>
+ </div>
+
+
+
+    </main>
+
+
+<?php include ('template-bottom.php'); ?>
