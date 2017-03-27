7e67599b9d9cafb1ed4002173012708ba6082cef
Kim Nobay <kim@nobay.co.uk>
2017-03-28 00:38:09 +0100

    Made footer dark with inverse color font

--- emma/footer.php
+++ emma/footer.php
@@ -3,9 +3,9 @@
 <footer data-role="footer" data-position="fixed" data-theme="a">
 
 <div class="ui-grid-b">
-    <div class="ui-block-a"><a href="destination.php"><i id="basket" class="fa fa-tourtuss"></i></a></div>
-     <div class="ui-block-b"><a href="search.php"><i class="fa fa-search"></i></a></div>
-    <div class="ui-block-c"><a href="profile.php"><i class="fa fa-user-o"></i><span class="footer-count">7</span></a></div>
+    <div class="ui-block-a"><a href="destination.php"><i id="basket" class="fa fa-tourtuss-inverse"></i></a></div>
+     <div class="ui-block-b"><a href="search.php"><i class="fa fa-search fa-inverse"></i></a></div>
+    <div class="ui-block-c"><a href="profile.php"><i class="fa fa-user-o fa-inverse"></i><span class="footer-count">7</span></a></div>
 </div>
 
 </footer>
