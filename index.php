de879ffc214b56c8da0b9dc97df97630e35f6966
Kim Nobay <kim@nobay.co.uk>
2017-03-27 16:19:11 +0100

    changed default release back to valentina

--- index.php
+++ index.php
@@ -4,17 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta http-equiv="refresh" content="5; url=./emma">
+    <meta http-equiv="refresh" content="5; url=./valentina">
     <title>Document</title>
 </head>
 <body>
     <h1>
-        Latest release will load in 5 seconds...
+        Latest stable release will load in 5 seconds...
     </h1>
     <h2>Releases available</h2>
         <ul>
             <li><a href="./emma">Emma</a></li>
-            <li><a href="./valentina">Valentina</a></li>
+            <li><a href="./valentina">Valentina (latest stable)</a></li>
             <li><a href="./justine">Justine</a></li>
         </ul>
 </body>
