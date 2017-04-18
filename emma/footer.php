63bbdbf153151caf4fcd3fed24ed5a1f8c613586
Kim Nobay <kim@nobay.co.uk>
2017-04-18 12:32:30 +0100

    Changed edit to cog and exclamation to number

--- emma/footer.php
+++ emma/footer.php
@@ -4,9 +4,9 @@
 <footer data-role="footer" data-position="fixed" data-theme="a">
 
 <div class="ui-grid-b">
-    <div class="ui-block-a"><a href="destination.php"><i id="basket" class="tourtuss-inverse"></i></a></div>
+    <div class="ui-block-a"><a href="destination.php"><i class="tourtuss-inverse"></i></a></div>
      <div class="ui-block-b"><a href="search.php"><i class="fa fa-search fa-inverse"></i></a></div>
-    <div class="ui-block-c"><a href="profile.php"><i class="fa fa-user-o fa-inverse"></i></a><span class="profileWarning">!</span></div>
+    <div class="ui-block-c"><a href="profile.php"><i class="fa fa-user-o fa-inverse"></i></a><span class="profileWarning">9</span></div>
 </div>
 
 
